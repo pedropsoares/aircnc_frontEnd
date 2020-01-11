b05d98056235a16373468c74696c1131f2334cc7
Pedro Soares <pedropaulos.1998@gmail.com>
2020-01-11 15:13:56 -0300

    ended

--- src/App.js
+++ src/App.js
@@ -1,39 +1,18 @@
-import React, { useState } from 'react';
-import api from './services/api';
+import React from 'react';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
+import Routes from './routes';
+
 function App() {
-  const [email, setEmail] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    console.log(email); 
-  }
-
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
 
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>telentos</strong> para sua empresa
-        </p> 
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-MAIL *</label>
-          <input 
-            type="email"
-            id="email"
-            placeholder="seu melhor email"
-            value={email}
-            onChange={event => setEmail(event.target.value)}
-          />
-
-          <button className="btn" type="submit">Entrar</button>
-        </form>
+        <Routes />
+        
       </div>
     </div>
   );
